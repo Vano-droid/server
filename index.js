e2995f1a0dfd36d15c6a8694da6ebd2ec008791c
vano-droid <103579175+Vano-droid@users.noreply.github.com>
2026-05-04 17:36:44 +0300

    update

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
+  cors: { origin: "*" }
 });
 
-// комнаты и игроки
+// комнаты (простая версия)
 const rooms = {};
 
 io.on("connection", (socket) => {
@@ -19,28 +17,24 @@ io.on("connection", (socket) => {
   socket.on("joinRoom", (roomId) => {
     socket.join(roomId);
 
-    if (!rooms[roomId]) {
-      rooms[roomId] = [];
-    }
+    if (!rooms[roomId]) rooms[roomId] = [];
 
     rooms[roomId].push(socket.id);
-
-    io.to(roomId).emit("playersUpdate", rooms[roomId]);
   });
 
   socket.on("gameAction", (data) => {
-    // пересылаем всем в комнате
     io.to(data.roomId).emit("gameUpdate", data);
   });
 
   socket.on("disconnect", () => {
     for (const roomId in rooms) {
       rooms[roomId] = rooms[roomId].filter(id => id !== socket.id);
-      io.to(roomId).emit("playersUpdate", rooms[roomId]);
     }
   });
 });
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
   console.log("Server running");
 });
